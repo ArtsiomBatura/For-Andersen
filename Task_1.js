ee5b8c8a5dbbf8f2ba5ffcfda1657e0c03b858c4
ArtsiomBatura <73168554+ArtsiomBatura@users.noreply.github.com>
2023-06-08 16:05:28 +0400

    Add files via upload

---
+++ Task_1.js
@@ -0,0 +1,6 @@
+let x = prompt('Input X: ')
+
+if ( x > 7){
+    console.log("Hello")
+}
+else alert('The number less than or equal to 7')
---
+++ Task_2.js
@@ -0,0 +1,6 @@
+var str = 'Вячеслав' 
+var input = prompt('Input Name: ')
+if (input === str){
+    console.log("Привет, " + input)
+}
+else alert('Нет такого имени') 
