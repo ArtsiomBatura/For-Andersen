5de6975abc1e3798aeec0da854331f7f0fdadf44
ArtsiomBatura <73168554+ArtsiomBatura@users.noreply.github.com>
2023-06-08 16:25:21 +0400

    Delete Task_3_Batura_Artiom_Language_Is_JavaScript.js

--- Task_3_Batura_Artiom_Language_Is_JavaScript.js
+++
@@ -1,7 +0,0 @@
-var arr = prompt("Введите элементы массива через пробел");
-var array = arr.split(" ").map(item => item.replace(/\s/g, ""));
-for (var i = 0; i < array.length; i++) {
-  if (parseInt(array[i]) % 3 === 0){
-    console.log(array[i]);
-  }
-}
